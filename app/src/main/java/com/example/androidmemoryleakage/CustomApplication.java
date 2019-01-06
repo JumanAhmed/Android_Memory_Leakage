6aed72cd3076179423ccaaed6e3bfc9763a7674a
jaki <jumanahmedjaki@gmail.com>
2019-01-07 01:34:37 +0600

    Setup LeakCanary and Test

---
+++ app/src/main/java/com/example/androidmemoryleakage/CustomApplication.java
@@ -0,0 +1,22 @@
+package com.example.androidmemoryleakage;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class CustomApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
+    }
+    
+}
